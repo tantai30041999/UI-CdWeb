a9d29f7506170ad64e263c3689fc2f0395f099e1
Thế Nguyễn Mai <thenguyen.mai@gmail.com>
2021-06-07 16:07:19 +0700

    update chat

--- src/components/main/MessageMain.js
+++ src/components/main/MessageMain.js
@@ -4,6 +4,7 @@ import RightMessage from './message/RightMessage';
 import Header from '../header/Header';
 import SlideBar from '../slidebar/SlideBar';
 import { ChatEngine } from 'react-chat-engine';
+import DirectChatPage from './message/DirectChatPage';
 class MessageMain extends Component {
   render() {
 
@@ -14,16 +15,14 @@ class MessageMain extends Component {
         <div className="main_content">
           <Header />
           <div className="container m-auto pt-5">
-            {/* <h1 className="font-semibold lg:mb-6 mb-3 text-2xl"> Messages</h1>
-            <div className="lg:flex lg:shadow lg:bg-white lg:space-y-0 space-y-8 rounded-md lg:-mx-0 -mx-5 overflow-hidden lg:dark:bg-gray-800">
+            <h1 className="font-semibold lg:mb-6 mb-3 text-2xl"> Messages</h1>
+            {/* <div className="lg:flex lg:shadow lg:bg-white lg:space-y-0 space-y-8 rounded-md lg:-mx-0 -mx-5 overflow-hidden lg:dark:bg-gray-800">
               <LeftMessage />
               <RightMessage />
             </div> */}
-            <ChatEngine
-                projectID='53f4a1bf-30a3-4aa8-849c-7e87f2491fee'
-                userName='admin@example.com'
-                userSecret='0e8717691cc36c56ed2e2cd5e3dba11e61de135c900d92e8d7e8d28ab46dd8b9'
-              />
+            <div className="lg:flex lg:shadow lg:bg-white lg:space-y-0 space-y-8 rounded-md lg:-mx-0 -mx-5 overflow-hidden lg:dark:bg-gray-800">
+            <DirectChatPage />
+            </div>
           </div>
         </div>
       </div>
